b306eb4d2398125b6c894233d7cb86e8deabf715
Mark Kanthiti <kanthitimark@gmail.com>
2026-03-01 15:43:49 +0200

    Fixed settings to use environment variables

--- college_site_new/settings.py
+++ college_site_new/settings.py
@@ -12,11 +12,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY
 # =====================================================
 
-SECRET_KEY = "django-insecure-very-secret-key-change-later"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "college-site-new.onrender.com").split(",")
 
 # =====================================================
 # APPLICATIONS
